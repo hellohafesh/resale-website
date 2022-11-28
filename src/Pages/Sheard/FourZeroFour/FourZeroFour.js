79473e63906a4552bcea35c1e0f0fa90f0038379
soumik825 <allhafesh825@gmail.com>
2022-11-28 21:34:32 +0600

    add 404 page for description

--- src/Pages/Sheard/FourZeroFour/FourZeroFour.js
+++ src/Pages/Sheard/FourZeroFour/FourZeroFour.js
@@ -7,11 +7,6 @@ const FourZeroFour = () => {
         <>
             <div className='body1'>
                 <div className='div1'>
-
-
-
-
-
                     <div className='div1'>
                         <svg className='ssvg' width="1123" height="837" viewBox="0 0 1123 837" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect width="1123" height="837" fill="black" />
